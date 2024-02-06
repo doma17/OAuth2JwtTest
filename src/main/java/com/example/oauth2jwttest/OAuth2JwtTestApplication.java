c0bcf11016b9b14df7d89f0c8db962af8629ce42
doma17 <rovin1273@gmail.com>
2024-02-06 18:44:00 +0900

    Initial commit

---
+++ src/main/java/com/example/oauth2jwttest/OAuth2JwtTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2jwttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2JwtTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2JwtTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oauth2jwttest/OAuth2JwtTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2jwttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2JwtTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
