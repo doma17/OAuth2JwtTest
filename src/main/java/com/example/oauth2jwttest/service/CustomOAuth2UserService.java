268d0fc898cf82569717d8ccc77f6f9d7cbde2c9
doma17 <rovin1273@gmail.com>
2024-03-10 15:04:09 +0900

    2

---
+++ src/main/java/com/example/oauth2jwttest/service/CustomOAuth2UserService.java
@@ -0,0 +1,25 @@
+package com.example.oauth2jwttest.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        log.info("[loadUser] oAuth2User : {}", oAuth2User);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+
+        return super.loadUser(userRequest);
+    }
+}
